f933f94da6a093251b1f7fff3c1a8b83240cd236
Eliaspty <grimaldo.elias@gmail.com>
2015-05-29 15:32:24 -0500

    updated

--- read_stochpy_oop.py
+++ read_stochpy_oop.py
@@ -1,66 +1,62 @@
-#!/usr/bin/python
 import re
 import itertools
 
-
-
-class ReadFile(object):
+class read(object):
+    
     def __init__(self):
-	self.filename = open('input_sample.txt', 'r')
+        self.species = {} 
+        self.k_rates = {}
+        
+    def get_file(self):
+        file = open('input_sample.txt', 'r')
+        return file
 
-    def list_species(self):
-        """ Create a list of the species from the input file
+    def get_list_species(self):
+        """ Create a list of the species from the input file                                                 
         """
-        file =self.filename.readlines()
+        #file = self.filename.readlines()
         rx_species = []
         s_no_duplicate = []
-        for lines in file:
+        species = []
+        for lines in self.get_file():
             if lines.find(">") > -1:
                 x= re.split("\W+", lines)
+                
                 rx_species.append(x)
-        all_species = list(itertools.chain(*rx_species))
+                #print "the rx", rx_species                                                                  
+                all_species = list(itertools.chain(*rx_species))
+                #print "after itertools", all_species                                                        
         filter_obj  = filter(None, all_species)    #filter the empty objects in the list
-        [s_no_duplicate.append(item) for item in filter_obj if item not in s_no_duplicate]
-
-        print "The list of species is:"
-        print s_no_duplicate #list of species without duplicates 
-        print "\n"
-
-    def species_values(self):
-        """Create a dictionary of the Species initial values from the input file
-        """
-        species = {}
-        file = open ('input_sample.txt', 'r')
-        for line in file:
-            if (line.find("k")== -1) and (line.find("=")> -1):
-                (key, value) = line.split("=")
-                species[str(key)] = value
-
-        species_values = {key.strip():value.strip() for key, value in species.iteritems()}
         
-        print "The species values are:"
-        print species_values
-        print "\n"
-
-    def constant_rates(self):
-        """Create a dictionary of the constant rates and values
+        [s_no_duplicate.append(item) for item in filter_obj if item not in s_no_duplicate]
+        
+        for i in s_no_duplicate:
+            y = re.sub('^[0-9]+', '', i)
+            species.append(y)
+        filter_obj2 = filter(None,species)
+        
+        return  s_no_duplicate, filter_obj2
+        
+    def get_species_values(self):
+        """Create a dictionary of the Species initial values from the input file                             
         """
-        k_rates = {}
-        file = open ('input_sample.txt', 'r')
-        for line in file:
-            if (line.find("k") > -1) and (line.find("=")> -1):
-                (key, value) = line.split("=")
-                k_rates[str(key)] = value
+       
+        for lines in self.get_file():
+            if (lines.find("k")== -1) and (lines.find("=")> -1):
+                (key, value) = lines.split("=")
+                self.species[str(key)] = value
 
-        rates_clean_spaces = {key.strip():value.strip() for key, value in k_rates.iteritems()}
-        print "The rates constants and values are:"
-        print rates_clean_spaces ##rates without the spaces    
+        species_values = {key.strip():value.strip() for key, value in self.species.iteritems()}
+        return species_values
 
+    def get_constant_rates(self):
+        """Create a dictionary of the constant rates and values                                              
+        """
+        
+        for lines in self.get_file():
+            if (lines.find("k") > -1) and (lines.find("=")> -1):
+                (key, value) = lines.split("=")
+                self.k_rates[str(key)] = value
 
-####################################################################################
-# Use the function
-
-test= read()
-test.list_species()
-test.species_values()
-test.constant_rates()
+        rates_clean_spaces = {key.strip():value.strip() for key, value in self.k_rates.iteritems()}
+        return rates_clean_spaces                                                
